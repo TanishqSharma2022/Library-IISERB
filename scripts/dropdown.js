33440adf3040cc45def3644e4f017088b740811c
Tanishq Sharma <tanishq22@iiserb.ac.in>
2024-05-14 01:32:36 +0530

    updated footer and carousel with responsiveness

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -2,6 +2,7 @@ const dropDown = [
   {
     id: 0,
     label: "About Us",
+    icon: "images/menubar/info-button.png",
     options: [
       {
         label: "About Us",
@@ -19,7 +20,89 @@ const dropDown = [
   },
   {
     id: 1,
-    label: "Vision",
+    label: "Services",
+    icon: "images/menubar/customer-support.png",
+
+    options: [
+      {
+        label: "First 1",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 2",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 3",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Print Resources",
+    icon: "images/menubar/printer.png",
+
+    options: [
+      {
+        label: "First 1",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 2",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 3",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "E-Resources",
+    icon: "images/menubar/device.png",
+
+    options: [
+      {
+        label: "First 1",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 2",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 3",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+  {
+    id: 4,
+    label: "Research Support",
+    icon: "images/menubar/research.png",
+
+    options: [
+      {
+        label: "First 1",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 2",
+        link: "https://www.google.com",
+      },
+      {
+        label: "First 3",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+  {
+    id: 5,
+    label: "Usage Stats",
+    icon: "images/menubar/pie-chart.png",
+
     options: [
       {
         label: "First 1",
@@ -44,11 +127,11 @@ dropDown.forEach((item) => {
 
   dropDownItem.innerHTML = `
 
-        <span class="flex items-center justify-between  gap-4  border-b   " >
-        
-        
+        <span class="flex items-center justify-between cursor-pointer gap-4 border-b   " >
+        <div class="flex gap-2 items-center">
+        <img src="${item.icon}" alt="icon" class="w-5 h-5" />
         ${item.label}
-
+</div>
 
 
         <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
